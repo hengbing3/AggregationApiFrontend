7befe9f2d628b8894c8249287b727c011210ca9b
Christer <hongweibin3@gmail.com>
2024-01-21 16:42:45 +0800

    commit

---
+++ config/routes.ts
@@ -0,0 +1,25 @@
+/*
+ * @Author: Christer hongweibin3@gmail.com
+ * @Date: 2024-01-21 15:07:32
+ * @LastEditors: Christer hongweibin3@gmail.com
+ * @LastEditTime: 2024-01-21 16:10:44
+ * @FilePath: \AggregationApiFrontend\config\routes.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export default [
+  { name: '登录', path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
+  { name: '欢迎页面',path: '/welcome', icon: 'smile', component: './Welcome' },
+  {
+    path: '/admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    name: '管理员页面',
+    routes: [
+      { path: '/admin', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', component: './Admin' },
+    ],
+  },
+  { icon: 'table', path: '/list', component: './TableList',  name: '表格页'},
+  { path: '/', redirect: '/welcome' },
+  { path: '*', layout: false, component: './404' },
+];
---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+/*
+ * @Author: Christer hongweibin3@gmail.com
+ * @Date: 2024-01-21 15:07:32
+ * @LastEditors: Christer hongweibin3@gmail.com
+ * @LastEditTime: 2024-01-21 16:07:48
+ * @FilePath: \AggregationApiFrontend\src\components\index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/**
+ * 这个文件作为组件的目录
+ * 目的是统一管理对外输出的组件，方便分类
+ */
+/**
+ * 布局组件
+ */
+import Footer from './Footer';
+import { Question } from './RightContent';
+import { AvatarDropdown, AvatarName } from './RightContent/AvatarDropdown';
+export { AvatarDropdown, AvatarName, Footer, Question };
