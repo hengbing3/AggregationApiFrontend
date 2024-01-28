211f6e94d335bf90894a2a345e50a151b4d9cc94
Christer <hongweibin3@gmail.com>
2024-01-28 15:59:36 +0800

    ✨ feat(config): 开发环境配置代理

--- config/proxy.ts
+++ config/proxy.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: Christer hongweibin3@gmail.com
+ * @Date: 2024-01-21 15:07:32
+ * @LastEditors: Christer hongweibin3@gmail.com
+ * @LastEditTime: 2024-01-28 15:55:26
+ * @FilePath: \AggregationApiFrontend\config\proxy.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /**
  * @name 代理的配置
  * @see 在生产环境 代理是无法生效的，所以这里没有生产环境的配置
@@ -11,16 +19,17 @@
  */
 export default {
   // 如果需要自定义本地开发服务器  请取消注释按需调整
-  // dev: {
-  //   // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
-  //   '/api/': {
-  //     // 要代理的地址
-  //     target: 'https://preview.pro.ant.design',
-  //     // 配置了这个可以从 http 代理到 https
-  //     // 依赖 origin 的功能可能需要这个，比如 cookie
-  //     changeOrigin: true,
-  //   },
-  // },
+  dev: {
+    // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
+    'http://localhost:8000/api/': {
+      // 要代理的地址
+      target: 'http://localhost:8101/api/',
+      // 配置了这个可以从 http 代理到 https
+      // 依赖 origin 的功能可能需要这个，比如 cookie
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    },
+  },
 
   /**
    * @name 详细的代理配置
