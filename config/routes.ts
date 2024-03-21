1318a9a0dc1f88f91f2b2fad6483c1f36c45932a
Christer <hongweibin3@gmail.com>
2024-03-21 23:12:42 +0800

    添加接口审核页面

--- config/routes.ts
+++ config/routes.ts
@@ -2,7 +2,7 @@
  * @Author: Christer hongweibin3@gmail.com
  * @Date: 2024-01-21 15:07:32
  * @LastEditors: Christer hongweibin3@gmail.com
- * @LastEditTime: 2024-02-16 22:26:35
+ * @LastEditTime: 2024-03-21 23:07:57
  * @FilePath: \AggregationApiFrontend\config\routes.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,7 +28,8 @@ export default [
         component: './Admin/InterfaceInfo',
         name: '接口管理',
       },
-      {icon: 'analysis', access: 'canAdmin', path: '/admin/interface_analysis', component: './Admin/InterfaceAnalysis', name: '接口分析'}
+      {icon: 'analysis', access: 'canAdmin', path: '/admin/interface_analysis', component: './Admin/InterfaceAnalysis', name: '接口分析'},
+      {icon: 'success', access: 'canAdmin', path: '/admin/interface_audit', component: './Admin/InterfaceAudit', name: '接口审核'}
       // { path: '/admin', redirect: '/admin/sub-page' },
       // { path: '/admin/sub-page', component: './Admin', name: "二级管理页" },
     ],
